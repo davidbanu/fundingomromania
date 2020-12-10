944d095878045720f2377e99a60020f0203996d7
davidbanu <davidbanu@ymail.com>
2020-12-10 22:47:12 +0200

    Changing data for projects

--- src/productData.js
+++ src/productData.js
@@ -4,7 +4,7 @@ export const productData = [
         category: "Ui Kits",
         link: "",
         style: {
-            backgroundColor: "rgb(42, 49, 198)"
+            backgroundColor: "rgb(0, 0, 0)"
         },
         description: "De la începutul acestui an, am început să slujim printre minoritățile de turci, tătari si romi din Dobrogea. În Cobadin avem deja o echipă de 8 misionari români și străini care lucrează neobosit printre aceste grupuri etnice. În vara acestui an am avut deja un botez Nou Testamentar, cu două persoane, printre musulmanii din această zonă. De asemenea, Dumnezeu ne-a deschis o ușă de misiune și spre musulmanii din Bulgaria (Dobrogea Bulgariei din partea de Nord-Est a țării până în Sud spre Turcia). Vara aceasta, în zona Burgas, un număr de 18 musulmani L-au mărturisit pe Domnul prin botez. Puteți ajuta lucrarea aceasta de misiune alăturându-vă bisericilor locale din zonă, în efortul lor de predicare a Evangheliei și formarea de ucenici ai lui Hristos printre oamenii (mai ales tinerii) cei mai puțin evanghelizați. De asemenea puteți face o donație care să acopere nevoile financiare ale echipei (aprox. 2500 Lei/ lună folosim doar pentru proiecte sociale: hrană și îmbrăcăminte pentru cei aflați în dificultăți materiale)."
     },
@@ -14,7 +14,7 @@ export const productData = [
         category: "Ui Kits",
         link: "/",
         style: {
-            backgroundColor: "rgb(102, 51, 153)"
+            backgroundColor: "rgb(0, 0, 0)"
         },
         description: "Recent am format o nouă echipa de misionari OM în București, care își propune să lucreze printre refugiații și printre evreii din România. Dorim să împărtășim dragostea lui Dumnezeu cu aceste grupuri etnice care sunt neglijate de cei mai mulți credincioși. De asemenea, intenționam să continuăm și lucrarea cu rromii din această zonă unde există multe localități fără nici o Biserică Evanghelică. Puteți veni alături de noi pentru a lucra la extinderea Împărăției lui Dumnezeu printre aceste categorii de oameni mai puțin expuși Evangheliei. De asemenea puteți face donații care să acopere nevoile materiale ale acestor oameni. De exemplu, de două ori pe an facem pachete de alimente pentru refugiați, ceea ce necesită o sumă totală de 22.000 Lei/an."
     },
@@ -34,7 +34,7 @@ export const productData = [
         category: "Templates",
         link: "/",
         style: {
-            backgroundColor: "rgb(210,38,48)"
+            backgroundColor: "rgb(0, 0, 0)"
         },
         description: "Programul nostru de instruire (MDT) este unul dintre cele mai importante lucrări de instruire în misiune, din această parte a Europei. În parteneriat cu OM Moldova instruim tineri romani și străini pentru lucrarea de misiune din Romania și de peste hotare. Însă, așa cum este lesne de înțeles, instruirea unui misionar, care este implicat total timp de 6 luni în această formă de instruire, este costisitoare și necesită foarte multă muncă și resurse diverse. Pentru un singur misionar este nevoie de 4000 Euro. Însă, așa cum vă puteți imagina, câștigul pentru Împărăția lui Dumnezeu, prin lucrarea acestor tineri, este incomensurabil.",
     },
@@ -50,11 +50,11 @@ export const productData = [
     },
 
     {
-        title: "Misiunea de susținere financiară și în rugăciune a misionarilor români*",
+        title: "Misiune de susținere administrativă a organizației OM din Ghimbav",
         category: "Templates",
         link: "/",
         style: {
-            backgroundColor: "rgb(42, 49, 198)"
+            backgroundColor: "rgb(0, 0, 0)"
         },
         description: "Pentru activitățile de instruire misionară și cele legate de misiune pe termen scurt, avem nevoie de un întreg aparat administrativ și de resurse financiare care să acopere cheltuielile de întreținere. Echipa de misionari din Ghimbav susține logistic lucrările din țară, asigură hrana necesară pentru misionarii aflați la bază, administrează sediul și îndeplinește toate aspectele legale pentru contabilitate. Încheiem anul cu un deficit de 5.500 Lei.",
     },
